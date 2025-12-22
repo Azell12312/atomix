3be8b49d2554a3ef8112ed2002eb608e4e6b4aa1
BiggestSeagull <biggestseagull@gmail.com>
2025-12-22 18:19:43 +0500

    backend v1

---
+++ Backend/atomix/src/main/java/com/students/atomix/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.students.atomix.controller;
+
+import com.students.atomix.dto.LoginRequestDTO;
+import com.students.atomix.dto.LoginResponseDTO;
+import com.students.atomix.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(
+            @RequestBody LoginRequestDTO request) {
+
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
---
+++ Backend/atomix/src/main/java/com/students/atomix/model/AppUser.java
@@ -0,0 +1,81 @@
+package com.students.atomix.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+
+
+@Entity
+@Table(name = "app_user")
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;   // ХРАНИМ ХЭШ, НЕ ЧИСТЫЙ ПАРОЛЬ
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false)
+    private String role; // ENGINEER / ADMIN
+
+    private Boolean active = true;
+
+    // getters / setters
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+}
